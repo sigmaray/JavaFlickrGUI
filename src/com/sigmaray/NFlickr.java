62b010ee7170ca7168f88241401e4266aae45b0c
sigmaray <sigmaray@gmail.com>
2016-07-23 12:41:13 +0300

    Working on NFlickr

--- src/com/sigmaray/NFlickr.java
+++ src/com/sigmaray/NFlickr.java
@@ -5,11 +5,16 @@
  */
 package com.sigmaray;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
@@ -18,6 +23,9 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+import java.awt.Desktop;
+import java.net.URI;
+
 /**
  *
  * @author John
@@ -25,16 +33,46 @@ import org.xml.sax.InputSource;
 public class NFlickr {
     static String API_KEY = "4cc2a6e2419deebfe86eca026cfda157";
     static String per_page = "20";
-    public static void main(String args[]) {
-        String s = getUrl("pizza", 1);
-        StringBuffer xo = fileGetContents(s);
-        System.out.println(xo);
-        parseXml(xo.toString());
+    
+    private static List photosList = new ArrayList();
+    
+    public static void printR(Object someObject) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(someObject));
     }
+    
+    public static void main(String args[]) {
+        String s = getUrl("pizza");
+        StringBuffer xo = fileGetContents(s);
+//        System.out.println(xo);
+        parseXml(xo.toString());
+//        printR(photosList);
+//        printR(buildImgUrl(photosList.get(0)));
+        String url = buildImgUrl((HashMap) photosList.get(0));
+//        System.out.println();
+        try {
+//            desktop.browse(new URI(url));
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (Exception  e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }        
+    }
+
+    public static String getUrl(String searchString) {
+        return getUrl(searchString, 0);
+    }
+
     public static String getUrl(String searchString, int page) {
         String url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" +
                 API_KEY + "&text=" + searchString + "&safe_search=1&page=" + Integer.toString(page) + "&per_page=" + per_page;
         return url;
+    }
+    
+    public static String buildImgUrl(HashMap item) {
+        String src = "http://farm" + item.get("farm") + ".static.flickr.com/" + item.get("server") + "/" + item.get("id") + "_" + item.get("secret") + "_m.jpg";
+//        String flickrUrl = "https://www.flickr.com/photos/" + item.get("owner") + "/" + item.get("id");
+        return src;
     }
 
     public static StringBuffer fileGetContents(String url) {
@@ -68,22 +106,34 @@ public class NFlickr {
          InputSource is = new InputSource(new StringReader(xmlString));
          Document doc = dBuilder.parse(is);
          doc.getDocumentElement().normalize();
-         System.out.println("Root element :" 
-            + doc.getDocumentElement().getNodeName());
+//         System.out.println("Root element :" 
+//            + doc.getDocumentElement().getNodeName());
          NodeList nList = doc.getElementsByTagName("student");
          
          NodeList nListZ = doc.getElementsByTagName("photo");
          for (int temp = 0; temp < nListZ.getLength(); temp++) {
             Node nNode = nListZ.item(temp);
-            System.out.println("\nCurrent Element :" 
-               + nNode.getNodeName());
+//            System.out.println("\nCurrent Element :" 
+//               + nNode.getNodeName());
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element eElement = (Element) nNode;
-                System.out.println("id : " 
-                     + eElement.getAttribute("id"));
+//                System.out.println("id : " 
+//                     + eElement.getAttribute("id"));
+//                
+//                System.out.println("owner : " 
+//                     + eElement.getAttribute("owner"));
                 
-                System.out.println("owner : " 
-                     + eElement.getAttribute("id"));
+                HashMap hm = new HashMap();
+                // Put elements to the map
+                hm.put("id", eElement.getAttribute("id"));
+                hm.put("owner", eElement.getAttribute("owner"));
+                hm.put("server", eElement.getAttribute("server"));
+                hm.put("farm", eElement.getAttribute("farm"));
+                hm.put("title", eElement.getAttribute("title"));
+                hm.put("secret", eElement.getAttribute("secret"));
+                
+                photosList.add(hm);
+//                System.out.println(photosList.size());
             }
          }
          
